83b070fb135423c92aee569d62e515263ccabd30
afzal <Afzal Shahul Hameed>
2016-06-02 00:54:03 +0530

    Integrates delete project

--- frontend/static/js/indvdual_project.js
+++ frontend/static/js/indvdual_project.js
@@ -13,6 +13,7 @@ function populate_page(project) {
     $('.project-desc').append("<p class='project-desc-heading'>Description:</p>");
     $('.project-desc').append(project.description);
     $('.project-desc-panel').show();
+    $('.delete-project').show();
 }
 
 function load_project(itemID) {
@@ -22,6 +23,28 @@ function load_project(itemID) {
         ).execute( function (response) {
             if (!response.error) {
                 populate_page(response);
+            } else {
+                if (response.error.code == 404) {
+                    $(".invalid-project-message").show();
+                } else {
+                    console.log(response.error);
+                }
+            }
+        });
+    } else {
+        $(".not-signed-in-alert").show();
+    }
+}
+
+function deleteProject(projectID) {
+    if (typeof gapi.client.ferris !== 'undefined') {
+        gapi.client.ferris.project.delete(
+            {'itemId': projectID}
+        ).execute( function (response) {
+            if (!response.error) {
+                $(".delete-alert").hide();
+                $(".project-details").hide();
+                $(".delete-project-success-message").show();
             } else {
                 console.log(response.error);
             }
@@ -33,4 +56,20 @@ function load_project(itemID) {
 
 $(document).ready( function () {
     $('.search-button').addClass('navigate-to-home');
+
+    $('.delete-project').click( function () {
+        $(".delete-alert").show();
+    });
+
+    $('.close-delete-alert').click( function () {
+        $(".delete-alert").hide();
+    });
+
+    $('.delete-project-confirm').click( function () {
+        deleteProject(itemID);
+    });
+
+    $('.back-home-after-delete').click( function () {
+        window.location.href = '/';
+    });
 });
